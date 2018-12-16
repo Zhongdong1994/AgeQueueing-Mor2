ce8da6f10a01eb5a182623d37512cb60d8d027e4
Zhongdong1994 <liuzhongdong12345@gmail.com>
2018-12-15 21:59:35 -0500

    Perfect version!

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ def simulate():
     '''
     # arrival_rates = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
     arrival_rates = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
-    Nuser = 100000
+    Nuser = 1000000
     mu = 1
-    rounds =1
+    rounds =10
     modes = ['FCFS', 'RANDOM','LCFS', 'PS', 'PLCFS', 'SJF', 'PSJF', 'SRPT','ABS','PABS']  ###  FB is currently unavailable
 
     for i in range(len(arrival_rates)):
